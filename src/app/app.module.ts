cd43bbee8937425e26c0f8b3bcf8ae2a8d2a0555
Wickedlolz <iceshotgg.linux@gmail.com>
2022-11-03 16:43:06 +0200

    add header, welcome and footer with html and css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent implements OnInit {
+  isAuth: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  handleLoginClick($event: MouseEvent): void {
+    $event.preventDefault();
+    this.isAuth = !this.isAuth;
+  }
+
+  handleLogOutClick(event: MouseEvent): void {
+    event.preventDefault();
+    this.isAuth = !this.isAuth;
+  }
+}
